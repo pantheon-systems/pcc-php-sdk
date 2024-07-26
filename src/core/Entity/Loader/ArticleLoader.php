1c81c486ab3d74b46c5c5ba55f20e975d68633a0
Al N <anitu@crowdfavorite.com>
2024-07-26 11:39:55 +0300

    add json parser for TREE_PANTHEON_V2 content type

--- src/core/Entity/Loader/ArticleLoader.php
+++ src/core/Entity/Loader/ArticleLoader.php
@@ -2,10 +2,12 @@
 
 namespace PccPhpSdk\core\Entity\Loader;
 
-use PccPhpSdk\api\Query\Enums\ContentType;
-use PccPhpSdk\api\Query\Enums\PublishingLevel;
+use Exception;
+use Parsedown;
 use PccPhpSdk\api\Query\ArticleQueryArgs;
 use PccPhpSdk\api\Query\ArticleSearchArgs;
+use PccPhpSdk\api\Query\Enums\ContentType;
+use PccPhpSdk\api\Query\Enums\PublishingLevel;
 use PccPhpSdk\core\Entity\Article;
 use PccPhpSdk\core\Entity\ArticlesList;
 use PccPhpSdk\core\PccClient;
@@ -13,234 +15,369 @@ use PccPhpSdk\core\Query\Builder\Article\ArticleQueryBuilder;
 use PccPhpSdk\core\Query\Builder\Article\ArticlesListQueryBuilder;
 use PccPhpSdk\core\Query\QueryInterface;
 
+use function htmlspecialchars;
+
 /**
  * Article Loader.
  */
-class ArticleLoader implements ArticleLoaderInterface {
+class ArticleLoader implements ArticleLoaderInterface
+{
 
-  /**
-   * PccClient.
-   *
-   * @var \PccPhpSdk\core\PccClient
-   */
-  protected PccClient $pccClient;
+    /**
+     * PccClient.
+     *
+     * @var PccClient
+     */
+    protected PccClient $pccClient;
 
-  /**
-   * Constructor for Content API.
-   *
-   * @param \PccPhpSdk\core\PccClient $pccClient
-   *   Preconfigured PccClient.
-   */
-  public function __construct(PccClient $pccClient) {
-    $this->pccClient = $pccClient;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function loadById(string $id, array $fields = [], PublishingLevel $publishingLevel = PublishingLevel::PRODUCTION, ?ContentType $contentType = null): ?Article {
-    $queryBuilder = new ArticleQueryBuilder();
-    $queryBuilder->addFields($this->getFields($fields));
-    $queryBuilder->filterById($id);
-    $queryBuilder->setPublishingLevel($publishingLevel);
-	if ($contentType) {
-	  $queryBuilder->setContentType($contentType);
-	}
-
-    $query = $queryBuilder->build();
-    $response = $this->sendRequest($query);
-    $response = $response['article'] ?: NULL;
-    return !empty($response) ? $this->toArticle($fields, $response, $contentType) : NULL;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function loadBySlug(string $slug, array $fields = [], PublishingLevel $publishingLevel = PublishingLevel::PRODUCTION): ?Article {
-    $queryBuilder = new ArticleQueryBuilder();
-    $queryBuilder->addFields($this->getFields($fields));
-    $queryBuilder->filterBySlug($slug);
-    $queryBuilder->setPublishingLevel($publishingLevel);
-
-    $query = $queryBuilder->build();
-    $response = $this->sendRequest($query);
-
-    $response = $response['article'] ?? NULL;
-    return !empty($response) ? $this->toArticle($fields, $response) : NULL;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function loadAll(?ArticleQueryArgs $queryArgs, ?ArticleSearchArgs $searchArgs, array $fields = []): ArticlesList {
-    $queryBuilder = new ArticlesListQueryBuilder();
-    $queryBuilder->addFields($this->getFields($fields));
-    if ($searchArgs) {
-      $queryBuilder->setFilter($searchArgs);
-    }
-    if ($queryArgs) {
-      $queryBuilder->setQueryArgs($queryArgs);
-    }
-    $query = $queryBuilder->build();
-    $response = $this->sendRequest($query);
-    $articles = $response['articles'] ?? [];
-    $articles_list = $this->toArticlesList($fields, $articles);
-
-    if (!empty($response['total'])) {
-      $articles_list->addTotalArticlesCount($response['total']);
-    }
-    if (!empty($response['cursor'])) {
-      $articles_list->addPageCursor($response['cursor']);
-    }
-    return $articles_list;
-  }
-
-  /**
-   * Send Request with query.
-   *
-   * @param \PccPhpSdk\core\Query\QueryInterface $query
-   *   Query for the request body.
-   *
-   * @return array
-   *   Response data as array.
-   */
-  private function sendRequest(QueryInterface $query): array {
-    $response = $this->pccClient->executeQuery($query);
-    $jsonResponse = json_decode($response, TRUE);
-
-    if ($jsonResponse === NULL) {
-      throw new \Exception("Failed to parse JSON response.");
-    }
-    if (!isset($jsonResponse['data']) || !is_array($jsonResponse['data'])) {
-      throw new \Exception("Invalid JSON response structure.");
+    /**
+     * Constructor for Content API.
+     *
+     * @param PccClient $pccClient
+     *   Preconfigured PccClient.
+     */
+    public function __construct(PccClient $pccClient)
+    {
+        $this->pccClient = $pccClient;
     }
 
-    $result = [];
+    /**
+     * {@inheritDoc}
+     */
+    public function loadById(
+        string $id,
+        array $fields = [],
+        PublishingLevel $publishingLevel = PublishingLevel::PRODUCTION,
+        ?ContentType $contentType = null
+    ): ?Article {
+        $queryBuilder = new ArticleQueryBuilder();
+        $queryBuilder->addFields($this->getFields($fields));
+        $queryBuilder->filterById($id);
+        $queryBuilder->setPublishingLevel($publishingLevel);
+        if ($contentType) {
+            $queryBuilder->setContentType($contentType);
+        }
 
-    if (!empty($jsonResponse)) {
-      if (!empty($jsonResponse['data']['article'])) {
-        $result['article'] = $jsonResponse['data']['article'];
-      }
-      if (!empty($jsonResponse['data']['articles'])) {
-        $result['articles'] = $jsonResponse['data']['articles'];
-      }
-      if (!empty($jsonResponse['extensions']['pagination']['total'])) {
-        $result['total'] = $jsonResponse['extensions']['pagination']['total'];
-      }
-      if (!empty($jsonResponse['extensions']['pagination']['cursor'])) {
-        $result['cursor'] = $jsonResponse['extensions']['pagination']['cursor'];
-      }
-    }
-    return $result;
-  }
-
-  /**
-   * Parse response to get ArticlesList.
-   *
-   * @param array $fields
-   *   The article fields.
-   * @param array $data
-   *   Response data.
-   *
-   * @return \PccPhpSdk\core\Entity\ArticlesList
-   *   ArticlesList entity.
-   */
-  private function toArticlesList(array $fields, array $data): ArticlesList {
-    $articlesList = new ArticlesList();
-    foreach ($data as $article) {
-      $articleEntity = $this->toArticle($fields, $article);
-      if ($articleEntity instanceof Article) {
-        $articlesList->addArticle($articleEntity);
-      }
+        $query = $queryBuilder->build();
+        $response = $this->sendRequest($query);
+        $response = $response['article'] ?: null;
+        return !empty($response) ? $this->toArticle($fields, $response, $contentType) : null;
     }
 
-    return $articlesList;
-  }
+    /**
+     * Get default article fields.
+     *
+     * @param array $fields
+     *   The Article fields.
+     *
+     * @return string[]
+     *   Array of fields.
+     */
+    protected function getFields(array $fields = []): array
+    {
+        if (!empty($fields)) {
+            return $fields;
+        }
 
-  /**
-   * Parse response data to get Article.
-   *
-   * @param array $fields
-   *   The article fields.
-   * @param array $data
-   *   Response data.
-   * @param ContentType $contentType
-   *   Response data.
-   *
-   * @return \PccPhpSdk\core\Entity\Article|null
-   *   Article entity or null.
-   */
-	private function toArticle(array $fields, array $data, ?ContentType $contentType = null): ?Article {
-    if (empty($data)) {
-      return NULL;
+        return [
+            'id',
+            'slug',
+            'title',
+            'siteId',
+            'tags',
+            'content',
+            'snippet',
+            'publishedDate',
+            'updatedAt',
+        ];
     }
 
-    $article = new Article();
-    foreach ($this->getFields($fields) as $field) {
-      switch ($field) {
-        case 'tags':
-          $article->{$field} = $data[$field] ?: [];
-          break;
+    /**
+     * Send Request with query.
+     *
+     * @param QueryInterface $query
+     *   Query for the request body.
+     *
+     * @return array
+     *   Response data as array.
+     */
+    private function sendRequest(QueryInterface $query): array
+    {
+        $response = $this->pccClient->executeQuery($query);
+        $jsonResponse = json_decode($response, true);
 
-        case 'content':
-        case 'snippet':
-		  // If content type is TREE_PANTHEON_V2, then don't parse
-		  if ($contentType == ContentType::TREE_PANTHEON_V2) {
-			$article->{$field} = $data[$field];
-		  }else{
-            $article->{$field} = $data[$field] ? $this->parseMarkdownToHtml($data[$field]) : '';
-		  }
-          break;
+        if ($jsonResponse === null) {
+            throw new Exception('Failed to parse JSON response.');
+        }
+        if (!isset($jsonResponse['data']) || !is_array($jsonResponse['data'])) {
+            throw new Exception('Invalid JSON response structure.');
+        }
 
-        default:
-          $article->{$field} = $data[$field] ?? '';
-      }
-    }
-    return $article;
-  }
+        $result = [];
 
-  /**
-   * Convert markdown format to html.
-   *
-   * @param string $content
-   *   The markdown string.
-   *
-   * @return string
-   *   Returns the html string.
-   */
-  private function parseMarkdownToHtml(string $content): string {
-    // Replace all occurrences of the pattern `{#h\..*}\n` with `\n`.
-    $pattern = '/{#h\..*}\n/';
-    $content = preg_replace($pattern, "\n", $content);
-    $parsedown = new \Parsedown();
-    return $parsedown->text($content);
-  }
-
-  /**
-   * Get default article fields.
-   *
-   * @param array $fields
-   *   The Article fields.
-   *
-   * @return string[]
-   *   Array of fields.
-   */
-  protected function getFields(array $fields = []): array {
-    if (!empty($fields)) {
-      return $fields;
+        if (!empty($jsonResponse)) {
+            if (!empty($jsonResponse['data']['article'])) {
+                $result['article'] = $jsonResponse['data']['article'];
+            }
+            if (!empty($jsonResponse['data']['articles'])) {
+                $result['articles'] = $jsonResponse['data']['articles'];
+            }
+            if (!empty($jsonResponse['extensions']['pagination']['total'])) {
+                $result['total'] = $jsonResponse['extensions']['pagination']['total'];
+            }
+            if (!empty($jsonResponse['extensions']['pagination']['cursor'])) {
+                $result['cursor'] = $jsonResponse['extensions']['pagination']['cursor'];
+            }
+        }
+        return $result;
     }
 
-    return [
-      'id',
-      'slug',
-      'title',
-      'siteId',
-      'tags',
-      'content',
-      'snippet',
-      'publishedDate',
-      'updatedAt',
-    ];
-  }
+    /**
+     * Parse response data to get Article.
+     *
+     * @param array $fields
+     *   The article fields.
+     * @param array $data
+     *   Response data.
+     * @param ContentType|null $contentType
+     *   Response data.
+     *
+     * @return Article|null
+     *   Article entity or null.
+     */
+    private function toArticle(array $fields, array $data, ?ContentType $contentType = null): ?Article
+    {
+        if (empty($data)) {
+            return null;
+        }
+
+        $article = new Article();
+        foreach ($this->getFields($fields) as $field) {
+            switch ($field) {
+                case 'tags':
+                    $article->{$field} = $data[$field] ?: [];
+                    break;
+
+                case 'content':
+                case 'snippet':
+                    if (!$data[$field]) {
+                        $article->{$field} = '';
+                        break;
+                    }
+                    if (ContentType::TREE_PANTHEON_V2 === $contentType) {
+                        $article->{$field} = $this->parseTreeToHtml($data[$field]);
+                        break;
+                    }
+                    $article->{$field} = $this->parseMarkdownToHtml($data[$field]);
+                    break;
+
+                default:
+                    $article->{$field} = $data[$field] ?? '';
+            }
+        }
+        return $article;
+    }
+
+    /**
+     * Parse TREE_PANTHEON_V2 response data to HTML.
+     *
+     * @param string $content
+     *
+     * @return string
+     */
+    private function parseTreeToHtml(string $content): string
+    {
+        $array = json_decode($content, true);
+        $class = 'scoped-' . substr(md5(mt_rand()), 0, 9);
+        $html = '';
+        $this->processNode($array, $html, $class);
+
+        return $this->createElement('div', ['class' => $class], [], $html);
+    }
+
+    /**
+     * Processes a JSON node and appends the corresponding HTML to the output by reference.
+     *
+     * @param array $node The JSON node to process.
+     * @param string &$html_output The accumulated HTML output (passed by reference).
+     * @param string $unique_class A unique class name used for scoping styles.
+     *
+     * @return void
+     */
+    private function processNode(array $node, string &$html_output, string $unique_class): void
+    {
+        $tag = $node['tag'] ?? 'div';
+        $data = $node['data'] ?? '';
+        $children = $node['children'] ?? [];
+        $style = $node['style'] ?? [];
+        $attrs = $node['attrs'] ?? [];
+
+        if (empty($children) && empty($data) && empty($attrs)) {
+            return;
+        }
+
+        if ($tag === 'style' && $data) {
+            $scoped_data = ".$unique_class $data";
+            $element = $this->createElement($tag, $attrs, $style, $scoped_data);
+            $html_output .= $element;
+            return;
+        }
+
+        $element = $this->createElement($tag, $attrs, $style, $data);
+
+        if (!empty($children)) {
+            $child_html = '';
+            foreach ($children as $child) {
+                $this->processNode($child, $child_html, $unique_class);
+            }
+            $element = str_replace('</' . $tag . '>', $child_html . '</' . $tag . '>', $element);
+        }
+
+        $html_output .= $element;
+    }
+
+    /**
+     * Creates an HTML element with the specified tag, attributes, styles, and content.
+     *
+     * @param string $tag The tag of the element to create.
+     * @param array $attrs Optional. An associative array of attributes for the element.
+     * @param array|string $styles Optional. An array or string of CSS styles to apply to the element.
+     * @param string|null $content Optional. The content to place inside the element.
+     * @return string The generated HTML element.
+     */
+    private function createElement(
+        string $tag,
+        array $attrs = [],
+        array|string $styles = [],
+        ?string $content = ''
+    ): string {
+        if (!$tag) {
+            $tag = 'div';
+        }
+
+        $element = '<' . $tag;
+
+        foreach ($attrs as $key => $value) {
+            $element .= ' ' . $key . '="' . htmlspecialchars($value) . '"';
+        }
+
+        $style_string = '';
+        if (is_array($styles)) {
+            foreach ($styles as $style) {
+                [$key, $value] = array_map('trim', explode(':', $style));
+                $style_string .= $key . ':' . $value . ';';
+            }
+        } elseif (is_array($styles)) {
+            foreach ($styles as $key => $value) {
+                $style_string .= $key . ':' . $value . ';';
+            }
+        }
+
+        if ($style_string) {
+            $element .= ' style="' . htmlspecialchars($style_string) . '"';
+        }
+
+        $element .= '>';
+
+        if ($content !== null) {
+            $element .= $content;
+        }
+
+        $element .= '</' . $tag . '>';
+
+        return $element;
+    }
+
+    /**
+     * Convert markdown format to html.
+     *
+     * @param string $content
+     *   The markdown string.
+     *
+     * @return string
+     *   Returns the html string.
+     */
+    private function parseMarkdownToHtml(string $content): string
+    {
+        // Replace all occurrences of the pattern `{#h\..*}\n` with `\n`.
+        $pattern = '/{#h\..*}\n/';
+        $content = preg_replace($pattern, "\n", $content);
+        $parsedown = new Parsedown();
+        return $parsedown->text($content);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function loadBySlug(
+        string $slug,
+        array $fields = [],
+        PublishingLevel $publishingLevel = PublishingLevel::PRODUCTION
+    ): ?Article {
+        $queryBuilder = new ArticleQueryBuilder();
+        $queryBuilder->addFields($this->getFields($fields));
+        $queryBuilder->filterBySlug($slug);
+        $queryBuilder->setPublishingLevel($publishingLevel);
+
+        $query = $queryBuilder->build();
+        $response = $this->sendRequest($query);
+
+        $response = $response['article'] ?? null;
+        return !empty($response) ? $this->toArticle($fields, $response) : null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function loadAll(
+        ?ArticleQueryArgs $queryArgs,
+        ?ArticleSearchArgs $searchArgs,
+        array $fields = []
+    ): ArticlesList {
+        $queryBuilder = new ArticlesListQueryBuilder();
+        $queryBuilder->addFields($this->getFields($fields));
+        if ($searchArgs) {
+            $queryBuilder->setFilter($searchArgs);
+        }
+        if ($queryArgs) {
+            $queryBuilder->setQueryArgs($queryArgs);
+        }
+        $query = $queryBuilder->build();
+        $response = $this->sendRequest($query);
+        $articles = $response['articles'] ?? [];
+        $articles_list = $this->toArticlesList($fields, $articles);
+
+        if (!empty($response['total'])) {
+            $articles_list->addTotalArticlesCount($response['total']);
+        }
+        if (!empty($response['cursor'])) {
+            $articles_list->addPageCursor($response['cursor']);
+        }
+        return $articles_list;
+    }
+
+    /**
+     * Parse response to get ArticlesList.
+     *
+     * @param array $fields
+     *   The article fields.
+     * @param array $data
+     *   Response data.
+     *
+     * @return ArticlesList
+     *   ArticlesList entity.
+     */
+    private function toArticlesList(array $fields, array $data): ArticlesList
+    {
+        $articlesList = new ArticlesList();
+        foreach ($data as $article) {
+            $articleEntity = $this->toArticle($fields, $article);
+            if ($articleEntity instanceof Article) {
+                $articlesList->addArticle($articleEntity);
+            }
+        }
+
+        return $articlesList;
+    }
 
 }
